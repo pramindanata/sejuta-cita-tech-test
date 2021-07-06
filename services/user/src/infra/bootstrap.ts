f8a027e72544b515e781b2677ef11fae87305cf2
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2021-07-06 14:45:46 +0800

    update bootstrap

--- services/user/src/infra/bootstrap.ts
+++ services/user/src/infra/bootstrap.ts
@@ -1,10 +1,19 @@
+import mongoose from 'mongoose';
 import { ConfigHelper } from './helpers';
 import { container } from './provider';
 import { createServer } from './server';
 
 export async function bootstrap(): Promise<void> {
-  const server = createServer();
   const configHelper = container.resolve(ConfigHelper);
+  const dbHost = configHelper.get('db.host');
+
+  await mongoose.connect(dbHost, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+
+  const server = createServer();
   const port = configHelper.get('app.port');
 
   server.listen(port, () => {
