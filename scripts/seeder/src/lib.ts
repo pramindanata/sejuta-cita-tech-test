1877c8740975dd6b04080c1927ea619eeb2ac795
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2021-07-17 20:44:42 +0800

    add global seed scripts

---
+++ scripts/seeder/src/lib.ts
@@ -0,0 +1,83 @@
+import { spawn } from 'child_process';
+
+export enum ExitCode {
+  One = 1,
+  Zero = 0,
+}
+
+export function getPodNames(
+  appLabels: string[],
+  podsInformation: string,
+): string[] {
+  const podNames: string[] = [];
+
+  for (const label of appLabels) {
+    const regex = new RegExp(`(${label}\\S+)`, 'g');
+    const match = podsInformation.match(regex);
+
+    if (!match || !match[0]) {
+      throw new PodNameForLabelNotFoundError(label);
+    }
+
+    podNames.push(match[0]);
+  }
+
+  return podNames;
+}
+
+export function executeCommand(command: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    let successText = '';
+    let errorText = '';
+
+    const process = spawn(command, {
+      shell: true,
+    });
+
+    process.on('close', (code) => {
+      if (code === 1) {
+        const error = new ProcessExitError(errorText);
+
+        return reject(error);
+      }
+
+      if (!successText) {
+        const error = new NoBufferOnProcessError();
+
+        return reject(error);
+      }
+
+      return resolve(successText);
+    });
+
+    process.stderr.on('data', (buffer) => {
+      errorText += buffer.toString();
+    });
+
+    process.stdout.on('data', (buffer) => {
+      successText += buffer.toString();
+    });
+  });
+}
+
+export class PodNameForLabelNotFoundError extends Error {
+  constructor(label: string) {
+    super(`Pod name for label "${label}" is not found.`);
+  }
+}
+
+export class NoBufferOnProcessError extends Error {
+  constructor() {
+    super(`Process is not returning any buffer, something wrong here.`);
+  }
+}
+
+export class ProcessExitError extends Error {
+  constructor(private errorText: string) {
+    super(`Process exit with code ${ExitCode.One}`);
+  }
+
+  getText(): string {
+    return this.errorText;
+  }
+}
---
+++ scripts/seeder/src/seed-run.ts
@@ -0,0 +1,22 @@
+import { executeCommand, ExitCode, getPodNames } from './lib';
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(ExitCode.One);
+});
+
+async function main(): Promise<void> {
+  const appLabels = ['auth-service', 'user-service'];
+  const getPodsInfoCommand = 'kubectl get pods';
+  const podsInformation = await executeCommand(getPodsInfoCommand);
+  const podNames = getPodNames(appLabels, podsInformation);
+
+  for (const podName of podNames) {
+    const seedCommand = `kubectl exec -it ${podName} -- npm run seed:run`;
+
+    // TODO: Replace pod name with label
+    console.log(`---Seeding ${podName}---`);
+    const outputText = await executeCommand(seedCommand);
+    console.log(outputText);
+  }
+}
